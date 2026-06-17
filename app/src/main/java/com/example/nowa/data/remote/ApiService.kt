ca385766fcd360d4f9965cfe89fb3c0087f02006
FX NOVRYANDHIKA WIDYANTO <novryandhykawidyanto@gmail.com>
2026-06-17 09:50:22 +0700

    final project

--- app/src/main/java/com/example/nowa/data/remote/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.example.nowa.data.remote
-
-import com.example.nowa.data.model.Post
-import retrofit2.http.GET
-
-interface ApiService {
-    @GET("posts")
-    suspend fun getPosts(): List<Post>
-}
